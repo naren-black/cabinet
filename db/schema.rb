1e8472b0998c7d327244cb3fca528c516217032f
Naren Nath <naren_black@yahoo.co.in>
2017-01-14 14:42:01 +1100

    Add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113054025) do
+ActiveRecord::Schema.define(version: 20170113055136) do
 
   create_table "docs", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
